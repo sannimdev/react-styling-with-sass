3b57936b5f0d8c27f3801089a371af50194ef337
Sannim <inegg.apps@gmail.com>
2020-07-23 13:35:10 +0900

    button에 property 지정하기 및 SCSS mixin 사용하기

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,8 +1,17 @@
 import React from "react";
+import classNames from "classnames";
 import "./Button.scss";
 
-function Button({ children }) {
-  return <button className="Button">{children}</button>;
+// size: large, medium, small
+// color: blue(default), pink, gray
+function Button({ children, size, color }) {
+  return <button className={classNames("Button", size, color)}>{children}</button>;
 }
+
+// 가장 정석적인 방법
+Button.defaultProps = {
+  size: "medium",
+  color: "blue",
+};
 
 export default Button;

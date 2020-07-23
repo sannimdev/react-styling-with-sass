0fda2fbf2389f0c760d0df46c0f4a0fa1b7a1d44
Sannim <inegg.apps@gmail.com>
2020-07-23 17:18:04 +0900

    Styled Component를 이용하여 버튼 만들기

--- src/App.js
+++ src/App.js
@@ -1,70 +1,100 @@
 import React, { useState } from "react";
-import Button from "./Components/Button";
+import styled from "styled-components";
 import "./App.scss";
-import CheckBox from "./Components/CheckBox";
-function App() {
-  const [check, setcheck] = useState(false);
-  const onChange = (e) => {
-    setcheck(e.target.checked);
-  };
-  return (
-    <div className="App">
-      <CheckBox onChange={onChange} checked={check}>
-        다음 약관에 모두 동의
-      </CheckBox>
+import StyledButton from "./Components/StyledButton";
+// import Button from "./Components/Button";
+// import CheckBox from "./Components/CheckBox";
 
-      {/* <div className="buttons">
-        <Button size="large">BUTTON</Button>
-        <Button>BUTTON</Button>
-        <Button size="small">BUTTON</Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="gray">
-          BUTTON
-        </Button>
-        <Button color="gray">BUTTON</Button>
-        <Button size="small" color="gray">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="pink">
-          BUTTON
-        </Button>
-        <Button color="pink">BUTTON</Button>
-        <Button size="small" color="pink">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="blue" outline>
-          BUTTON
-        </Button>
-        <Button color="gray" outline={true}>
-          BUTTON
-        </Button>
-        <Button size="small" color="pink" outline={true}>
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button color="blue" fullWidth size="large" className="customized-button">
-          BUTTON
-        </Button>
-        <Button color="gray" fullWidth={true} size="large">
-          BUTTON
-        </Button>
-        <Button
-          color="pink"
-          fullWidth={true}
-          size="large"
-          onClick={() => console.log("클릭")}
-          onMouseMove={() => console.log("moving")}
-        >
-          BUTTON
-        </Button>
-      </div> */}
-    </div>
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+// const Circle = styled.div`
+//   width: 5rem;
+//   height: 5rem;
+//   background: ${(props) => props.color || "black"};
+//   border-radius: 50%;
+
+//   ${(props) =>
+//     props.huge &&
+//     css`
+//       width: 10rem;
+//       height: 10rem;
+//     `}
+// `;
+
+function App() {
+  // const [check, setcheck] = useState(false);
+  // const onChange = (e) => {
+  //   setcheck(e.target.checked);
+  // };
+  return (
+    <AppBlock>
+      <StyledButton>Button</StyledButton>
+    </AppBlock>
+    // <div className="App">
+    //   {/* <Circle />
+    //   <Circle color="blue" huge /> */}
+    //   {/* <CheckBox onChange={onChange} checked={check}>
+    //     다음 약관에 모두 동의
+    //   </CheckBox> */}
+
+    //   {/* <div className="buttons">
+    //     <Button size="large">BUTTON</Button>
+    //     <Button>BUTTON</Button>
+    //     <Button size="small">BUTTON</Button>
+    //   </div>
+    //   <div className="buttons">
+    //     <Button size="large" color="gray">
+    //       BUTTON
+    //     </Button>
+    //     <Button color="gray">BUTTON</Button>
+    //     <Button size="small" color="gray">
+    //       BUTTON
+    //     </Button>
+    //   </div>
+    //   <div className="buttons">
+    //     <Button size="large" color="pink">
+    //       BUTTON
+    //     </Button>
+    //     <Button color="pink">BUTTON</Button>
+    //     <Button size="small" color="pink">
+    //       BUTTON
+    //     </Button>
+    //   </div>
+    //   <div className="buttons">
+    //     <Button size="large" color="blue" outline>
+    //       BUTTON
+    //     </Button>
+    //     <Button color="gray" outline={true}>
+    //       BUTTON
+    //     </Button>
+    //     <Button size="small" color="pink" outline={true}>
+    //       BUTTON
+    //     </Button>
+    //   </div>
+    //   <div className="buttons">
+    //     <Button color="blue" fullWidth size="large" className="customized-button">
+    //       BUTTON
+    //     </Button>
+    //     <Button color="gray" fullWidth={true} size="large">
+    //       BUTTON
+    //     </Button>
+    //     <Button
+    //       color="pink"
+    //       fullWidth={true}
+    //       size="large"
+    //       onClick={() => console.log("클릭")}
+    //       onMouseMove={() => console.log("moving")}
+    //     >
+    //       BUTTON
+    //     </Button>
+    //   </div> */}
+    // </div>
   );
 }
 
---
+++ src/Components/StyledButton.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  /* 공통 스타일 */
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  /* 크기 */
+  height: 2.25rem;
+  line-height: 2.25rem;
+  font-size: 1rem;
+
+  /* 색상 */
+  background: #228be6;
+  &:hover {
+    background: #339af0;
+  }
+  &:active {
+    background: #1c7ed6;
+  }
+
+  /* 기타 */
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+function StyledButton({ children, ...rest }) {
+  return <Button {...rest}>{children}</Button>;
+}
+
+export default StyledButton;

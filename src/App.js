a413b23267bb0f87b6b685c36445f29609b8624c
Sannim <inegg.apps@gmail.com>
2020-07-23 17:18:28 +0900

    App.js 주석 제거

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import "./App.scss";
 import StyledButton from "./Components/StyledButton";
-// import Button from "./Components/Button";
-// import CheckBox from "./Components/CheckBox";
 
 const AppBlock = styled.div`
   width: 512px;
@@ -13,88 +11,11 @@ const AppBlock = styled.div`
   padding: 1rem;
 `;
 
-// const Circle = styled.div`
-//   width: 5rem;
-//   height: 5rem;
-//   background: ${(props) => props.color || "black"};
-//   border-radius: 50%;
-
-//   ${(props) =>
-//     props.huge &&
-//     css`
-//       width: 10rem;
-//       height: 10rem;
-//     `}
-// `;
-
 function App() {
-  // const [check, setcheck] = useState(false);
-  // const onChange = (e) => {
-  //   setcheck(e.target.checked);
-  // };
   return (
     <AppBlock>
       <StyledButton>Button</StyledButton>
     </AppBlock>
-    // <div className="App">
-    //   {/* <Circle />
-    //   <Circle color="blue" huge /> */}
-    //   {/* <CheckBox onChange={onChange} checked={check}>
-    //     다음 약관에 모두 동의
-    //   </CheckBox> */}
-
-    //   {/* <div className="buttons">
-    //     <Button size="large">BUTTON</Button>
-    //     <Button>BUTTON</Button>
-    //     <Button size="small">BUTTON</Button>
-    //   </div>
-    //   <div className="buttons">
-    //     <Button size="large" color="gray">
-    //       BUTTON
-    //     </Button>
-    //     <Button color="gray">BUTTON</Button>
-    //     <Button size="small" color="gray">
-    //       BUTTON
-    //     </Button>
-    //   </div>
-    //   <div className="buttons">
-    //     <Button size="large" color="pink">
-    //       BUTTON
-    //     </Button>
-    //     <Button color="pink">BUTTON</Button>
-    //     <Button size="small" color="pink">
-    //       BUTTON
-    //     </Button>
-    //   </div>
-    //   <div className="buttons">
-    //     <Button size="large" color="blue" outline>
-    //       BUTTON
-    //     </Button>
-    //     <Button color="gray" outline={true}>
-    //       BUTTON
-    //     </Button>
-    //     <Button size="small" color="pink" outline={true}>
-    //       BUTTON
-    //     </Button>
-    //   </div>
-    //   <div className="buttons">
-    //     <Button color="blue" fullWidth size="large" className="customized-button">
-    //       BUTTON
-    //     </Button>
-    //     <Button color="gray" fullWidth={true} size="large">
-    //       BUTTON
-    //     </Button>
-    //     <Button
-    //       color="pink"
-    //       fullWidth={true}
-    //       size="large"
-    //       onClick={() => console.log("클릭")}
-    //       onMouseMove={() => console.log("moving")}
-    //     >
-    //       BUTTON
-    //     </Button>
-    //   </div> */}
-    // </div>
   );
 }
 
